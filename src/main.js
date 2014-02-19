a7bdfd523aa99a3e254eb11486f945d4494cdb6c
Azrius <alexscottclark@outlook.com>
2014-02-19 11:13:17 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ function init(){
     $("#menu_button").click(function(){
         open_menu();
     });
+    
+    document.getElementById("menu_button").webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
 }
 
 $(document).ready(function(){
     init();
+    webkitRequestFullScreen();
 });
