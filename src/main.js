a3dd18c2222712e81438cab37ad98f8a5b5f25c9
Azrius <alexscottclark@outlook.com>
2014-02-19 11:05:30 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,16 +6,6 @@ function init(){
     $("#menu_button").click(function(){
         open_menu();
     });
-    
-    mouse_cursor();
-}
-
-function mouse_cursor(){
-    $(document).mousemove(function(e){
-       $("#cursor").css("position","absolute");
-       
-       $("#cursor").css('left', e.clientX + 0.1).css('top', e.clientY + 0.1);
-    });
 }
 
 $(document).ready(function(){
