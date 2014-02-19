f468eed3d0249a1cf694bea3911e342ce2420809
Azrius <alexscottclark@outlook.com>
2014-02-19 10:53:55 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ function init(){
 
 function mouse_cursor(){
     $(document).mousemove(function(e){
-       $('#cursor').css('left', e.clientX - 20).css('top', e.clientY + 7);
+       $("#cursor").css("color", "red");
     });
 }
 
