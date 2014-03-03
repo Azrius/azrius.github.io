efe2f7ff54ef967cc92ccd14bff2ac548c9642e8
Azrius <alexscottclark@outlook.com>
2014-03-03 11:47:04 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-stats = {};
+stats = {
+  citizens: 100,
+  lumberjacks: 2,
+  oilriggers: 2,
+  steelworkers: 2,
+};
+
 resources = {
   crude_oil: 0,
   lumber: 0,
+  steel: 0,
+}
+
+function update_resources(){
+  var resource_panel = document.getElementById("resources");
+  
+  resource_panel.innerHTML = "Crude oil: " + resources.crude_oil;
+}
+
+function update_stats(){
+  
 }
 
 function tick(){
