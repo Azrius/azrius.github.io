6b0a9fc167fa515b5023126eb231237716f619db
Azrius <alexscottclark@outlook.com>
2014-03-03 09:05:59 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,3 @@
-function open_menu(){
+function init(){
     
 }
-
-function init(){
-    $("#menu_button").click(function(){
-        open_menu();
-    });
-}
-
-$(document).ready(function(){
-    init();
-    alert(httpGet("http://example.com/"));
-});
-
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
