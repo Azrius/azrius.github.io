ef7eef21710dd7744e97e7c34d7333829ddaf532
Azrius <alexscottclark@outlook.com>
2014-03-05 10:20:45 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,7 +15,11 @@ function update_resources(){
   var resource_panel = document.getElementById("resources");
   var stats_panel = document.getElementById("stats");
   
-  resource_panel.innerHTML = "Crude oil: " + resources.crude_oil + "<br />" + "Lumber: " + resources.lumber;
+  stats.crude_oil += stats.oilriggers * 0.5;
+  stats.lumber += stats.lumberjacks * 0.25;
+  stats.steel += stats.steelworkers * 0.75;
+  
+  resource_panel.innerHTML = "Crude oil: " + resources.crude_oil + "<br />" + "Lumber: " + resources.lumber + "<br />" + "Steel: " + resources.steel;
   stats_panel.innerHTML = "Citizens: " + stats.citizens + "<br />" + "Lumberjacks: " + stats.lumberjacks + "<br />" + "Oilriggers: " + stats.oilriggers + "<br />" + "Steelworkers: " + stats.steelworkers;
 }
 
